1627c846143fa3f7a504d76f745efd487e5b1b18
tonny187 <tonyoanthony@gmail.com>
2024-11-13 22:15:38 -0400

    adding 3 new modules

---
+++ anotations3.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+def process_salary(salary: Union[int, float]) -> float:
+    """
+    Procesa un salario que puede ser entero o flotante y lo devuelve como flotante.
+
+    Parámetros:
+    salary (Union[int, float]): Un salario que puede ser entero o flotante.
+
+    Retorna:
+    float: El salario convertido a flotante
+    """
+
+    return float(salary)
