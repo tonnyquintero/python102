52ea92f2396504cd3f176ed37294fe252b69fd6d
tonny187 <tonyoanthony@gmail.com>
2024-11-18 22:34:04 -0400

    adding 2 new clases1

---
+++ class.py
@@ -0,0 +1,12 @@
+class Order:
+    global_discount = 10
+
+    def __init__(self, amount):
+        self.amount = amount
+
+    @classmethod
+    def update_global_discount(cls, new_discount):
+        cls.global_discount = new_discount
+
+Order.update_global_discount(15)
+print(Order.global_discount)
